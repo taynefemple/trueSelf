a975932f65cfce858925ec5435a62d085ada7201
Wayne Temple <waynetemple@gmail.com>
2018-12-11 22:10:32 -0600

    version 1.0 complete!

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ let background;
 let settings;
 let close;
 
+// is this necessary?
 chrome.runtime.sendMessage({ action: 'getSettings' }, (res) => {
   settings = res;
 });
@@ -13,11 +14,34 @@ const updateHandler = () => {
   settings = {
     oldName: oldName.value,
     newName: newName.value,
-    enabled: true
+    enabled: !!submit.checked
   };
   background.settings.oldName = settings.oldName;
   background.settings.newName = settings.newName;
+  background.settings.enabled = settings.enabled;
   chrome.storage.sync.set(settings);
+};
+
+const onInputHandler = (bool) => {
+  submit.checked = bool;
+  if (bool) {
+    chrome.browserAction.setIcon({
+      path: {
+        16: 'images/flagSmall.png',
+        48: 'images/flagMedium.png',
+        128: 'images/flagLarge.png'
+      }
+    });
+  } else {
+    chrome.browserAction.setIcon({
+      path: {
+        128: 'disabledIcon.png'
+      }
+    });
+    oldName.value = '';
+    newName.value = '';
+  }
+  updateHandler();
 };
 
 const loadHandler = () => {
@@ -27,45 +51,18 @@ const loadHandler = () => {
   close = document.querySelector('.close');
   background = chrome.extension.getBackgroundPage();
 
-  newName.addEventListener('keyup', () => {
-    updateHandler();
-  }, false);
+
+  newName.addEventListener('keyup', () => onInputHandler(true), false);
   newName.value = background.settings.newName;
 
-  oldName.addEventListener('keyup', () => {
-    updateHandler();
-  });
+  oldName.addEventListener('keyup', () => onInputHandler(true), false);
   oldName.value = background.settings.oldName;
-  submit.checked = background.settings.enabled;
 
-  submit.addEventListener('click', () => {
-    if (!submit.checked) {
-      chrome.browserAction.setIcon({
-        path: {
-          128: 'disabledIcon.png'
-        }
-      });
-      submit.checked = false;
-      background.settings.enabled = false;
-      background.settings.oldName = '';
-      background.settings.newName = '';
-    }
-    else {
-      chrome.browserAction.setIcon({
-        path: {
-          16: 'images/flagSmall.png',
-          48: 'images/flagMedium.png',
-          128: 'images/flagLarge.png'
-        }
-      });
-      submit.checked = true;
-      background.settings.enabled = true;
-    }
-  });
+  submit.addEventListener('click', () => onInputHandler(!!submit.checked));
+  submit.checked = background.settings.enabled;
 
   close.addEventListener('click', () => window.close());
 }
-
 
 // init
 document.addEventListener('DOMContentLoaded', loadHandler);
